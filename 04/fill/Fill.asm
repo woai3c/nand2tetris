34da72ebd352a9bb180924ba37ffd8cc527e5366
woai3c <411020382@qq.com>
2018-09-27 16:50:34 +0800

    添加代码

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,8 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -11,4 +6,70 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+    // 24576为键盘的地址 16384-24575 刚好8K 为屏幕的地址
+    @24575
+    D = A
+
+    // R0存储屏幕最大地址
+    @0
+    M = D
+
+    // R1存储屏幕当前地址
+    @SCREEN
+    D = A
+    @1
+    M = D
+(LOOP)
+    @KBD
+    D = M
+    @FILL
+    D;JGT
+
+    @CLEAR
+    0;JMP
+(FILL)
+    // 判断屏幕是否为满
+    @0
+    D = M
+    @1
+    D = D - M
+    @LOOP
+    D;JEQ
+
+    @1
+    // 缓存当前地址
+    D = M
+    // 将当前地址推入A
+    A = M
+    // 将地址对应的屏幕位置变黑
+    M = -1
+
+    // 当前地址+1
+    @1
+    M = D + 1
+
+    @LOOP
+    0;JMP
+(CLEAR)
+    // 判断屏幕是否为空
+    @SCREEN
+    D = A
+    @1
+    D = D - M
+    @LOOP
+    D;JEQ
+
+    @1
+    // 缓存当前地址
+    D = M
+    // 将当前地址推入A
+    A = M
+    // 将地址对应的屏幕位置变白
+    M = 0
+
+    // 当前地址-1
+    @1
+    M = D - 1
+
+    @LOOP
+    0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -1,9 +1,29 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// 3*2 换算成 2个3相加
+    // 初始化
+    @2
+    M = 0
 
-// Put your code here.
+    // 判断是否小于0
+    @0
+    D = M
+    @END
+    D;JLT
+(LOOP)
+    @1
+    M = M - 1
+    D = M
+    @END
+    D;JLT
+
+    @0
+    D = M
+    @2
+    M = M + D
+
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
